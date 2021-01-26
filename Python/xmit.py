67842e551cbe776a3c939a6e77f07446e27ddcce
KALTCHUK <71845372+KALTCHUK@users.noreply.github.com>
2021-01-26 18:02:42 +0200

    Create xmit.py

---
+++ Python/xmit.py
@@ -0,0 +1,18 @@
+# This program opens an ASCII file and transmits it through a COM port to the Z80 MC.
+
+import serial
+
+# Which COM port?
+print('\r\n')    
+
+com_port = input("COM port? ")
+Z80_port = serial.Serial(port = "COM" + str(com_port), baudrate = 9600)
+
+file_name = input("File name? ")
+f = open(file_name,"rb")
+print('\r\n' + "Transmitting..." + '\r\n')    
+    
+Z80_port.write(f.read()+b'\r\n')
+print("Transmission complete.")
+
+f.close()
