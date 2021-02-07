9a66018b73a3fb45f8db80be50fb44b35ebb992e
KALTCHUK <71845372+KALTCHUK@users.noreply.github.com>
2021-02-07 12:38:55 +0200

    a

--- tests/RECEIVE.asm
+++ tests/RECEIVE.asm
@@ -10,10 +10,7 @@
 ; When the end of file has been reached, an EOT (4) is sent, followed by the CRC,
 ; also in ASCII (2 bytes).
 ; RECEIVE writes the last block, closes the file and sends an ACK if all went OK.
-; Otherwise, it sends a NAK (21) followed by the error code. 
-;
-; 	01 = CRC error
-;	02 = disk error
+; Otherwise, it sends a NAK (21).
 ;==================================================================================
 TPA			.EQU	100H
 REBOOT		.EQU	0H
@@ -28,225 +25,130 @@ F_CLOSE		.EQU	16
 F_WRITE		.EQU	21
 F_DELETE	.EQU	19
 
-CR			.EQU	0DH
-LF			.EQU	0AH
+STX			.EQU	02H
 EOT			.EQU	04H
 ACK			.EQU	06H
+LF			.EQU	0AH
+CR			.EQU	0DH
 NAK			.EQU	015H
 		
 FCB			.EQU	05CH
 BUFF		.EQU	080H
 ;==================================================================================
 			.ORG TPA
-			
-			CALL GETCHAR			; Get Drive letter (A...P)
-			SUB	040H				; convert from ASCII letter to byte
+			LD	A,ACK				; Ok, I'm alive. You can start communication.
+			CALL PUTCHAR
+
+NEWFILE:	CALL GETCHAR			; Get Drive letter (A...P)
+			SUB	040H				; convert from ASCII letter to byte (A=1, B=2...)
 			LD	HL,FCB
 			LD	(HL),A
 			INC	HL
 			LD	B,11
-NEXT1:		CALL GETCHAR			; Get 11 chars (8 from name + 3 type = NNNNNNNN.TTT)
+NEXT1:		PUSH BC
+			CALL GETCHAR			; Get 11 chars,8 from name + 3 type (NNNNNNNN.TTT)
 			LD	(HL),A
 			INC	HL
+			POP	BC
 			DEC	B
 			JR	NZ,NEXT1
 			LD	A,0
 			LD (HL),A				; Extension = 0
 
 			LD	A,0					; Set initial parameters
-			LD	(buffPos),A
 			LD	(checkSum),A
-			LD	(byteCount),A
 			LD	HL,BUFF
 			LD	(buffPtr),HL
 			
 			LD	C,F_DELETE			; Delete file
 			LD	DE,FCB
-			CALL	BDOS
+			CALL BDOS
 
 			LD	C,F_MAKE			; Create file
 			LD	DE,FCB
-			CALL	BDOS
-;==================================================================================
-; Start debugging from here >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-;==================================================================================
+			CALL BDOS
+			
+			LD	A,ACK				; Tell SEND to start xmit archive.
+			CALL PUTCHAR
 
-									; Start receiving the archive
-GETHEX:		CALL GETCHR
+GETHEX:		CALL GETCHAR				; Start receiving the archive
 			CP	EOT
 			JR	Z,CLOSE
-			LD   B,A
+			LD	B,A
 			PUSH BC
-			CALL GETCHR
-			POP BC
-			LD   C,A
-
+			CALL GETCHAR
+			POP	BC
+			LD	C,A
 			CALL BCTOA
 
-	LD	B,A
-	LD	A,(checkSum)
-	ADD	A,B
-	LD	(checkSum),A
-	LD	A,(byteCount)
-	INC	A
-	LD	(byteCount),A
-
-	LD	A,B
-
-	LD	HL,(buffPtr)
-
-	LD	(HL),A
-	INC	HL
-	LD	(buffPtr),HL
-
-	LD	A,(buffPos)
-	INC	A
-	LD	(buffPos),A
-	CP	80H
-
-	JR	NZ,NOWRITE
-
-	LD	C,WRITES
-	LD	DE,FCB
-	CALL	BDOS
-	LD	A,'.'
-	CALL	PUTCHR
-
-        ; New line every 8K (64 dots)
-	LD	A,(printCount)
-	INC	A
-	CP	64
-	JR	NZ,noCRLF
-	LD	(printCount),A
-	LD	A,CR
-	CALL	PUTCHR
-	LD	A,LF
-	CALL	PUTCHR
-	LD	A,0
-noCRLF:	LD	(printCount),A
-
-	LD	HL,BUFF
-	LD	(buffPtr),HL
-
-	LD	A,0
-	LD	(buffPos),A
-NOWRITE:
-	JR	GETHEX
-	
-
-CLOSE:
-
-	LD	A,(buffPos)
-	CP	0
-	JR	Z,NOWRITE2
-
-	LD	C,WRITES
-	LD	DE,FCB
-	CALL	BDOS
-	LD	A,'.'
-	CALL	PUTCHR
-
-NOWRITE2:
-	LD	C,CLOSEF
-	LD	DE,FCB
-	CALL	BDOS
-
-; Byte count (lower 8 bits)
-	CALL 	GETCHR
-	LD   B,A
-	PUSH BC
-	CALL GETCHR
-	POP BC
-	LD   C,A
-
-	CALL BCTOA
-	LD	B,A
-	LD	A,(byteCount)
-	SUB	B
-	CP	0
-	JR	Z,byteCountOK
-
-	LD	A,CR
-	CALL	PUTCHR
-	LD	A,LF
-	CALL	PUTCHR
-
-	LD	DE,countErrMess
-	LD	C,PSTRING
-	CALL	BDOS
-
-	; Sink remaining 2 bytes
-	CALL GETCHR
-	CALL GETCHR
-
-	JR	FINISH
-
-byteCountOK:
-
-; Checksum
-	CALL 	GETCHR
-	LD   B,A
-	PUSH BC
-	CALL GETCHR
-	POP BC
-	LD   C,A
-
-	CALL BCTOA
-	LD	B,A
-	LD	A,(checkSum)
-	SUB	B
-	CP	0
-	JR	Z,checksumOK
-
-	LD	A,CR
-	CALL	PUTCHR
-	LD	A,LF
-	CALL	PUTCHR
-
-	LD	DE,chkErrMess
-	LD	C,PSTRING
-	CALL	BDOS
-	JR	FINISH
-
-checksumOK:
-	LD	A,CR
-	CALL	PUTCHR
-	LD	A,LF
-	CALL	PUTCHR
-
-	LD	DE,OKMess
-	LD	C,PSTRING
-	CALL	BDOS
-		
-
-
-FINISH:
-	LD	C,SETUSR
-	LD	E,0
-	CALL	BDOS
-
-	JP	REBOOT
-
-
-
-
-;==================================================================================
-; Send [CR][LF] to console
-;==================================================================================
-CRLF:		LD	C,C_WRITE
-			LD	E,CR
-			CALL BDOS
-			LD	C,C_WRITE
-			LD	E,LF
-			CALL BDOS
-			RET
+			LD	B,A					; Update checksum
+			LD	A,(checkSum)
+			ADD	A,B
+			LD	(checkSum),A
 			
+			LD	A,B					; Put received byte in buffer
+			LD	HL,(buffPtr)
+			LD	(HL),A
+			INC	HL
+			LD	(buffPtr),HL
+			LD	A,H
+			CP	1					; Check if we reached the end of the buffer
+			JR	NZ,GETHEX
+
+			LD	C,F_WRITE			; We got a full buffer. Write it on disk.
+			LD	DE,FCB
+			CALL BDOS
+
+			LD	HL,BUFF				; Reset buffer.
+			LD	(buffPtr),HL
+			
+			LD	A,ACK				; Tell SEND to resume transmission.
+			CALL PUTCHAR
+			JR	GETHEX
+
+CLOSE:		LD	BC,BUFF				; Check if buffer is empty
+			LD	HL,(buffPtr)
+			SCF
+			CCF
+			SBC	HL,BC
+			JR	Z,BEMPTY
+
+			LD	C,F_WRITE			; Write the last block to file.
+			LD	DE,FCB
+			CALL BDOS
+
+BEMPTY:		LD	C,F_CLOSE			; Close the file.
+			LD	DE,FCB
+			CALL BDOS
+
+			CALL GETCHAR			; Get checksum
+			LD   B,A
+			PUSH BC
+			CALL GETCHAR
+			POP BC
+			LD   C,A
+			CALL BCTOA
+			LD	B,A
+			LD	A,(checkSum)
+			SUB	B
+			CP	0
+			LD	A,ACK
+			JR	Z,CHECKOK
+			LD	A,NAK
+CHECKOK:	CALL PUTCHAR
+
+			CALL GETCHAR
+			CP	STX
+			JP	Z,NEWFILE
+
+			JP	REBOOT
+
 ;==================================================================================
 ; Wait for a char into A (no echo)
 ;==================================================================================
 GETCHAR:	LD	E,$FF
 			LD 	C,C_RAWIO
-			CALL 	BDOS
+			CALL BDOS
 			CP	0
 			JR	Z,GETCHAR
 			RET
@@ -259,13 +161,29 @@ PUTCHAR:	LD C,C_WRITE
 			CALL BDOS
 			RET
 
-
-
 ;==================================================================================
-
-buffPos		.DB	0H
+; Convert ASCII characters in BC to a byte in A
+;==================================================================================
+BCTOA:		LD   A,B				
+			SUB  $30
+			CP   $0A
+			JR   C,BCTOA1
+			SUB  $07
+BCTOA1:		RLCA
+			RLCA
+			RLCA
+			RLCA
+			LD   B,A
+			LD   A,C
+			SUB  $30
+			CP   $0A
+			JR   C,BCTOA2
+			SUB  $07
+BCTOA2:		ADD  A,B
+			RET
+			
+;==================================================================================
 buffPtr		.DW	0000H
 checkSum 	.DB	0H
-byteCount 	.DB	0H
 
 			.END
