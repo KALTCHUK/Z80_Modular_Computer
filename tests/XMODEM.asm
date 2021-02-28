c41916bc7ebd060b7c777b3afff3ee32c9d597d9
KALTCHUK <71845372+KALTCHUK@users.noreply.github.com>
2021-02-28 14:07:10 +0200

    A

--- tests/XMODEM.asm
+++ tests/XMODEM.asm
@@ -24,6 +24,8 @@ F_DMAOFF	.EQU	26
 SOH			.EQU	01H
 EOT			.EQU	04H
 ACK			.EQU	06H
+LF			.EQU	0AH
+CR			.EQU	0DH
 NAK			.EQU	015H
 CAN			.EQU	018H
 SUB			.EQU	01AH
@@ -41,18 +43,19 @@ DMA			.EQU	080H
 			LD	C,C_STRING
 			CALL BDOS
 			JP	REBOOT
-START:		LD	HL,DMA				; Initialize variables
+START:		LD	HL,DMA				; Initialize buffer pointer
 			LD	(BUFPTR),HL
 			LD	A,0
-			LD	(RETRY),A
+			LD	(RETRY),A			; Initialize retry counter
 			INC	A
-			LD	(BLOCK),A
+			LD	(BLOCK),A			; Initialize block counter
 			CALL DELFILE			; Delete file
 			CALL MAKEFILE			; Create file
 			CP	4					; 0, 1, 2 or 3 = OK
 			JP	M,ALIVE
 CANCEL:		CALL SENDCAN
 			JP	REBOOT
+
 ALIVE:		CALL SENDNAK
 GETCHAR:	CALL CONIN				; Get 1st char
 			CP	EOT					; Is it the end?
@@ -62,28 +65,31 @@ GETCHAR:	CALL CONIN				; Get 1st char
 			LD	A,0
 			LD	(CHKSUM),A			; Reset checksum
 			LD	HL,DMA
-			LH	(BUFPTR),HL			; Reset buffer pointer
+			LD	(BUFPTR),HL			; Reset buffer pointer
 			CALL CONIN				; Get block number
 			LD	B,A
 			CALL CONIN				; Get /block number
 			ADD	A,B
 			CP	0FFH
-			JR	NZ, AGAIN
+			JR	NZ, CANCEL
 			LD	A,(BLOCK)
 			CPL
 			CP	B
 			JR	Z,GOOD2GO
+			JR	CANCEL
+			
 AGAIN:		LD	A,(RETRY)
 			INC	A
 			LD	(RETRY),A			; Increment retry counter
 			CP	4
 			JP	P,CANCEL
 			JR	ALIVE
+
 GOOD2GO:	CALL CONIN
 			LD	HL,(BUFPTR)
 			LD	(HL),A				; Put received byte in buffer
 			INC	HL
-			LD	(BUFPTR),HL
+			LD	(BUFPTR),HL			; Increment buffer pointer
 			LD	B,A
 			LD	A,(CHKSUM)
 			ADD	A,B
@@ -91,7 +97,7 @@ GOOD2GO:	CALL CONIN
 			LD	A,H
 			CP	1					; Check if we reached the end of the buffer (0FFh is the last valid position)
 			JR	NZ,GOOD2GO
-			CALL CONIN
+			CALL CONIN				; Receive checksum
 			LD	B,A
 			LD	A,(CHKSUM)
 			CP	B
@@ -109,11 +115,7 @@ GOOD2GO:	CALL CONIN
 			CALL SENDACK
 			JR	GETCHAR
 			
-CLOSE:		CALL SENDNAK
-			CALL CONIN
-			CP	EOT
-			JR	NZ,CANCEL
-			CALL SENDACK
+CLOSE:		CALL SENDACK
 			CALL CLOSFILE
 			JP	REBOOT
 			
@@ -189,9 +191,6 @@ BC2A1:		RLCA
 			JR   C,BC2A2
 			SUB  $07
 BC2A2:		ADD  A,B
-
-			CALL LOG
-			
 			RET
 			
 ;==================================================================================
