c3dfecfa1db87a706b2d10f52598a9d647f2c0c3
KALTCHUK <71845372+KALTCHUK@users.noreply.github.com>
2021-03-21 16:05:48 +0200

    A

--- tests/liner.asm
+++ tests/liner.asm
@@ -89,19 +89,19 @@ MAXLBUF		.EQU	DMA+80
 			CALL CONOUT
 			LD	C,'>'
 			CALL CONOUT
-WAITCHAR:	CALL CONIN
+WAITCHAR:	CALL CONIN					; Wait for user's charater.
 			CP	CAN
-			JR	Z,GOTCAN
+			JR	Z,GOTCAN				; Is it <CAN>? (= delete line).
 			CP	CR
-			JR	Z,GOTCR
+			JR	Z,GOTCR					; Is it <ENTER>?
 			CP	BS
-			JR	Z,GOTBS
-			LD	HL,(LBUFPTR)
-			LD	BC,MAXLBUF
+			JR	Z,GOTBS					; Is it <BS>? (= backspace).
+			LD	HL,(LBUFPTR)			; None of the above options, so let's put it in the buffer.
+			LD	BC,MAXLBUF				; But 1st, we have to check if we still have space.
 			SCF
 			CCF
 			SBC	HL,BC
-			JR	Z,LBUFFULL
+			JR	Z,LBUFFULL				; Is buffer full?
 			LD	HL,(LBUFPTR)
 			LD	(HL),A
 			INC	HL
@@ -110,32 +110,36 @@ WAITCHAR:	CALL CONIN
 OUTWAIT:	CALL CONOUT
 			JR	WAITCHAR
 
-LBUFFULL:	LD	C,BEL
+LBUFFULL:	LD	C,BEL					; Buffer is full. Just ring the bell.
 			JR	OUTWAIT
 
-GOTBS:		LD	D,1
+GOTBS:		LD	D,1						; We got a backspace.
 AFTGOTBS:	CALL BSPROC
 			JR	WAITCHAR
 
-GOTCR:		LD	HL,(LBUFPTR)
-			LD	A,0
+GOTCR:		LD	HL,(LBUFPTR)			; We got an ENTER, which means the the user
+			LD	A,0						; has funished typing the command line.
 			LD	(HL),A
 			LD	C,CR
 			CALL CONOUT
 			LD	C,LF
-			CALL CONOUT
-			CALL UPPER					; Convert line to uppercase before parsing
+			CALL CONOUT					; Output <CR><LF>.
+			CALL UPPER					; Convert line to uppercase before parsing.
 			LD	HL,CMDTBL
 			LD	DE,DMA
-			CALL PARSER					; Find command comparing with Command Table
+			CALL PARSER					; Find command comparing buffer with Command Table.
 			INC	A
-			JP	Z,UNKNOWN
-			JP	(HL)
+			JP	Z,UNKNOWN				; No match found in command table.
+			JP	(HL)					; Jump to Command Routine
+			
 
-GOTCAN:		LD	D,0
+GOTCAN:		LD	D,0						; We got a line delete.
 			JR	AFTGOTBS
 
-BSPROC:		LD	HL,(LBUFPTR)			; Routine to do the backspace and line delete
+;================================================================================================
+; Routine to do the backspace and line delete. D=1, backspace; D=0, delete line.
+;================================================================================================
+BSPROC:		LD	HL,(LBUFPTR)
 			LD	BC,DMA
 			SCF
 			CCF
@@ -159,7 +163,10 @@ LBUFEMPTY:	LD	C,BEL
 			CALL CONOUT
 			RET
 			
-UPPER:		LD	HL,DMA-1				; Routine to convert line to upper case
+;================================================================================================
+; Routine to convert line buffer content to upper case
+;================================================================================================
+UPPER:		LD	HL,DMA-1
 NEXT2UP:	INC	HL
 			LD	A,(HL)
 			CP	0
@@ -174,12 +181,13 @@ NEXT2UP:	INC	HL
 			
 ;================================================================================================
 ; Parse command. HL=cmd_table_pointer, DE=line_buffer_pointer.
-; Returns jump address on HL. regA=FFh if no match.
+; Returns jump address on HL. regA=cmd_num or FFh if no match.
 ;================================================================================================
-PARSER:		LD	A,0
-			LD	(CMDNUM),A		; Init command number
+PARSER:		PUSH BC
+			LD	A,0
+			LD	(CMDNUM),A		; Init command number.
 			PUSH DE
-			POP	IX				; Backup line buffer pointer
+			POP	IX				; Backup line buffer pointer.
 NEXT2PARS:	LD	A,(DE)
 			CP	(HL)
 			JR	NZ,NOTEQU
@@ -188,49 +196,91 @@ NEXT2PARS:	LD	A,(DE)
 			JR	NEXT2PARS
 NOTEQU:		LD	A,(HL)
 			CP	RS
-			
+			JR	Z,ISRS
+			CP	ETX
+			JR	NZ,NEXTCMD
+ISRS:		LD	A,(DE)
+			CP	0
+			JR	Z,ISZERO
+			CP	' '
+			JR	NZ,NEXTCMD
+ISZERO:		LD	A,(HL)
+			CP	ETX
+			JR	Z,CMDMATCH
+			INC	HL
+			JR	ISZERO
+CMDMATCH:	INC	HL
+			PUSH HL
+			POP	DE
+			LD	H,0
+			LD	A,(CMDNUM)
+			LD	L,A
+			PUSH HL
+			POP	BC
+			ADD	HL,BC			; command_number * 2
+			ADD	HL,BC			; command_number * 3
+			ADD HL,DE
+			POP	BC
+			RET
+NEXTCMD:	LD	A,(HL)
+			CP	RS
+			JR	Z,ISRS2
+			CP	ETX
+			JR	Z,NOMATCH
+			INC	HL
+			JR	NEXTCMD
+ISRS2:		INC	HL
+			LD	A,(CMDNUM)
+			INC	A
+			LD	(CMDNUM),A
+			PUSH IX
+			POP	DE
+			JR	NEXT2PARS
+NOMATCH:	LD	HL,0
+			LD	A,0FFH
+			POP	BC
+			RET
 
 ;================================================================================================
 ; Memory Operations
 ;================================================================================================
 MEMO:		CALL PRINTSEQ
-			.DB	">Memory Operations",0
+			.DB	"M>Ready for Memory Operations",0
 			JP	WBOOT
 
 ;================================================================================================
 ; Xmodem Command
 ;================================================================================================
 XMODEM:		CALL PRINTSEQ
-			.DB	">Xmodem [d:]file",0
+			.DB	">XMODEM aaaa",0
 			JP	WBOOT
 
 ;================================================================================================
 ; Hexadecimal to Executable conversion
 ;================================================================================================
 HEX2COM:	CALL PRINTSEQ
-			.DB	">Hex2com aaaa",0
+			.DB	">HEX2COM aaaa",0
 			JP	WBOOT
 
 ;================================================================================================
 ; LCD Operations
 ;================================================================================================
 LCD:		CALL PRINTSEQ
-			.DB	">LCD Operations",0
+			.DB	"L>Ready for LCD Operations",0
 			JP	WBOOT
 
 ;================================================================================================
 ; Disk Operations
 ;================================================================================================
 DISK:		CALL PRINTSEQ
-			.DB	">Disk Operations",0
+			.DB	"D>Ready for Disk Operations",0
 			JP	WBOOT
 
 ;================================================================================================
 ; Execute Command
 ;================================================================================================
-RUN:		CALL PRINTSEQ
-			.DB	">Run aaaa",0
-			JP	WBOOT
+RUN:		CALL GETBYTE		
+			HALT
 
 ;================================================================================================
 ; Unknown Command message. HL has the address of the line buffer.
@@ -244,6 +294,102 @@ UNKNOWN:	LD	C,(HL)
 			LD	C,'?'
 			CALL CONOUT
 			JP	WBOOT
+
+;================================================================================================
+; Get word from command line. DE=line_buffer_pointer(should point to where byte starts).
+; If successfull, return word in BC. A=0 if missing arg, A=1 if OK, A=2 if invalid arg. 
+;================================================================================================
+GETWORD:	CALL GETBYTE
+			CP	1
+			RET	NZ
+			LD	C,B
+			INC	DE
+			CALL GETBYTE
+			CP	1
+			RET NZ
+			LD	A,B
+			LD	B,C
+			LD	C,A
+			LD	A,1
+			RET
+			
+;================================================================================================
+; Get byte from command line. DE=line_buffer_pointer(should point to where byte starts).
+; If successfull, return byte in regC. A=0 if missing arg, A=1 if OK, A=2 if invalid arg. 
+;================================================================================================
+GETBYTE:	LD	A,(DE)
+			CP	0
+			JR	Z,GBNA				; End of buffer and no arg found.
+			CP	' '
+			JR	Z,GBSPC				; Trim the space.
+			LD	H,A
+			CALL ISITHEX
+			CP	1
+			JR	NZ,GBIA				; Invalid arg.
+			INC	DE
+			LD	A,(DE)
+			LD	L,A
+			CALL ISITHEX
+			CP	1
+			JR	NZ,GBIA				; Invalid arg.
+			CALL HL2B				; Convert ASCII pair to byte
+			LD	A,1
+			RET
+GBNA:		LD	A,0
+			RET
+GBSPC:		INC	DE
+			JR	GETBYTE
+GBIA:		LD	A,2
+			RET
+
+ISITHEX:	CP	'G'
+			JP	P,NOTHEX
+			CP	'A'
+			JP	P,ISHEX
+			CP	040H
+			JP	P,NOTHEX
+			CP	'0'
+			JP	P,ISHEX
+NOTHEX:		LD	A,0
+			RET
+ISHEX:		LD	A,1
+			RET
+
+;================================================================================================
+; Convert ASCII to HEX (HL --> B)
+;================================================================================================
+HL2B:		PUSH BC
+			LD	A,060H
+			SUB	H
+			LD	C,057H
+			JP	C,DISCOUNT
+			LD	A,040H
+			SUB	H
+			LD	C,037H
+			JP	C,DISCOUNT
+			LD	C,030H
+DISCOUNT:	LD	A,H
+			SUB	C
+CONVL:		LD	B,A
+			SLA	B
+			SLA	B
+			SLA	B
+			SLA	B
+			LD	A,060H
+			SUB	L
+			LD	C,057H
+			JP	C,DISCOUNT2
+			LD	A,040H
+			SUB	L
+			LD	C,037H
+			JP	C,DISCOUNT2
+			LD	C,030H
+DISCOUNT2:	LD	A,L
+			SUB	C
+			OR	B
+			POP	BC
+			LD	B,A
+			RET
 
 ;================================================================================================
 CMDTBL:		.DB	"MEMO",RS
@@ -260,7 +406,8 @@ JMPTBL:		JP	MEMO
 			JP	DISK
 			JP	RUN
 			
-LBUFPTR		.DW		0
+CMDNUM		.DB	0
+LBUFPTR		.DW	0
 
 
 			.END
