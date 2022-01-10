185d220ad44c689c395c324e2ea405719f4c5bb7
KALTCHUK <71845372+KALTCHUK@users.noreply.github.com>
2022-01-10 11:23:35 +0200

    Update LBR.py

--- Python/LBR.py
+++ Python/LBR.py
@@ -4,7 +4,7 @@ import string
 def ShowUse():
     print("LBR v1.0 Library file extractor.")
     print("Commands:")
-    print("   O <library>  open library .LBR")
+    print("   O <library>  open library (.LBR implicit)")
     print("   L            list files")
     print("  eXtract       extract all files")
     print("   Q            quit program.\n")
@@ -20,6 +20,7 @@ while True:
         print(file_name + " opened")
 
     if command[0] == "l" or command[0] == "L":
+        fp.seek(0)
         record = fp.read(32)
         start = record[12] + (16*record[13])
         entries  = 4*(record[14] + (16*record[15]))-1
@@ -35,10 +36,36 @@ while True:
                     name = name + chr(record[i])
                 for i in range(9,12):
                     ext = ext + chr(record[i])
-                print(x, name + "." + ext + " -->", size, " records start at ", start)
+                full_name = name.rstrip(" ") + "." + ext.rstrip(" ") 
+                print(x, full_name)
 
     if command[0] == "x" or command[0] == "X":
-        print("extract")
+        fp.seek(0)
+        record = fp.read(32)
+        start = record[12] + (16*record[13])
+        entries  = 4*(record[14] + (16*record[15]))-1
+        print(entries, " records in directory.\n")
+        for x in range(0,entries):
+            record = fp.read(32)
+            if record[0] == 0:
+                start = record[12] + (16*record[13])
+                size = record[14] + (16*record[15])
+                name = ""
+                ext  = ""
+                for i in range(1,9):
+                    name = name + chr(record[i])
+                for i in range(9,12):
+                    ext = ext + chr(record[i])
+                full_name = name.rstrip(" ") + "." + ext.rstrip(" ") 
+                print("Extracting " + full_name)
+                fp_target = open(full_name, "wb")
+                old_fp_position = fp.tell()
+                fp.seek(128 * start, 0)
+                for i in range(0,size):
+                    record = fp.read(128)
+                    fp_target.write(record)
+                fp_target.close()
+                fp.seek(old_fp_position,0)
 
     if command[0] == "q" or command[0] == "Q":
         break
