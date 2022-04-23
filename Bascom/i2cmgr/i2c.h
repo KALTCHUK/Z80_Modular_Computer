4e54c05d3e256c693e194e16ff98154eb711e6cd
KALTCHUK <71845372+KALTCHUK@users.noreply.github.com>
2022-04-23 20:26:39 +0300

    Update i2c.h

--- Bascom/i2cmgr/i2c.h
+++ Bascom/i2cmgr/i2c.h
@@ -1,81 +1,55 @@
 /* I2C.H - Header file for I2C Card read/write fuctions */
 /* 20/04/22, by Kaltchuk                                */
 
-#define STOP	0
-#define START	1
+#define WRITE	1
+#define READ	2
+#define RREAD	3
 
-/* Primitive functions */
-i2c_stop(i2c_card)
-char	i2c_card; {
-	outp(i2c_card,STOP);
-}
+/* Primitive functions 					*/
 
-i2c_start(i2c_card)
-char	i2c_card; {
-	outp(i2c_card,START);
-}
-
-i2c_wr(i2c_card, byte)
-char	i2c_card, byte; {
-	outp(i2c_card+1, byte);
-}
-
-char i2c_rd_ack(i2c_card)
-char	i2c_card; {
-	char	byte;
-
-	byte = inp(i2c_card);
-	return byte;
-}
-
-char i2c_rd_nak(i2c_card)
-char	i2c_card; {
-	char	byte;
-
-	byte = inp(i2c_card+1);
-	return byte;
-}
-
-/* High level functions */
-writeI2C(i2c_card, slave_addr, address, num_bytes, buf)
-char	i2c_card, slave_addr, *buf, num_bytes;
-int	address; {
+/* Write function					*/
+I2Cwr(i2c_card, slave, size_addr, addr, num_bytes, buf)
+char	i2c_card, slave, size_addr, num_bytes, buf[];
+int	addr; {
 	char	i;
 
-	i2c_start();
-	i2c_wr(i2c_card,slave_addr<<1);
-	if(address > 0xff) {
-		i2c_wr(i2c_card, address>>8);
-	}
-	i2c_wr(i2c_card, address && 0xff);
+	outp(i2c_card, WRITE);
+	outp(i2c_card, slave);
+	outp(i2c_card, size_addr);
+	if(size_addr == 2)
+		outp(i2c_card, addr >> 8);
+	outp(i2c_card, addr && 0xff);
+	outp(i2c_card, num_bytes);
 	for(i=0; i<num_bytes; i++)
-		i2c_wr(i2c_card, buf[i]);
-	i2c_stop();
+		outp(i2c_card, buf[i]);
 }
 
-readI2C(i2c_card, slave_addr, num_bytes, buf)
-char	i2c_card, slave_addr, *buf, num_bytes; {
+/* Read function					*/
+I2Crd(i2c_card, slave, num_bytes, buf)
+char	i2c_card, slave, num_bytes, buf[]; {
 	char	i;
 
-	i2c_start();
-	i2c_wr(i2c_card,(slave_addr<<1)+1);
-	for(i=0; i<num_bytes-1; i++)
-		buf[i] = i2c_rd_ack(i2c_card);
-	buf[i] = i2c_rd_nak(i2c_card);
-	i2c_stop();
+	outp(i2c_card, READ);
+	outp(i2c_card, slave);
+	outp(i2c_card, num_bytes);
+	for(i=0; i<num_bytes; i++)
+		buf[i] = inp(i2c_card);
 }
 
-read_randI2C(i2c_card, slave_addr, address, num_bytes, buf)
-char	i2c_card, slave_addr, *buf, num_bytes;
-int	address; {
+/* Read random function					*/
+I2Crr(i2c_card, slave, size_addr, addr, num_bytes, buf)
+char	i2c_card, slave, size_addr, num_bytes, buf[];
+int	addr; {
 	char	i;
 
-	i2c_start();
-	i2c_wr(i2c_card,slave_addr<<1);
-	if(address > 0xff) {
-		i2c_wr(i2c_card, address>>8);
-	}
-	i2c_wr(i2c_card, address && 0xff);
-	readI2C(i2c_card, slave_addr, num_bytes, buf);
+	outp(i2c_card, RREAD);
+	outp(i2c_card, slave);
+	outp(i2c_card, size_addr);
+	if(size_addr == 2)
+		outp(i2c_card, addr >> 8);
+	outp(i2c_card, addr && 0xff);
+	outp(i2c_card, num_bytes);
+	for(i=0; i<num_bytes; i++)
+		buf[i] = inp(i2c_card);
 }
-
+
