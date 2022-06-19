c82ec9f2b2ac5cc45996ee5ae16b67c6ca829212
KALTCHUK <71845372+KALTCHUK@users.noreply.github.com>
2022-06-19 17:22:51 +0300

    diskboot update

---
+++ tasm32/diskboot.asm
@@ -0,0 +1,165 @@
+;==================================================================================
+; Contents of this file are copyright Grant Searle
+;
+; You have permission to use this for NON COMMERCIAL USE ONLY
+; If you wish to use it elsewhere, please include an acknowledgement to myself.
+;
+; http://searle.hostei.com/grant/index.html
+;
+; eMail: home.micros01@btinternet.com
+;
+; If the above don't work, please perform an Internet search to see if I have
+; updated the web page hosting service.
+;
+; Customized by Kaltchuk for use with Z80 Modular Computer, june/2022
+;
+;==================================================================================
+BIOS			.EQU	0E620H
+WBOOT			.EQU	+03H		; Entry point for  function WBOOT
+CONST			.EQU	+06H		; Entry point for  function CONST
+CONIN			.EQU	+09H		; Entry point for  function CONIN
+CONOUT			.EQU	+0CH		; Entry point for  function CONOUT
+
+loadAddr		.EQU	0D000h
+numSecs			.EQU	24	; Number of 512 sectors to be loaded
+
+; FLASH card stuff
+FLASH_ADDR		.EQU	0B0H			; Base I/O address for compact flash card
+; CF registers
+CF_DATA			.EQU	(FLASH_ADDR+0)	; R/W
+CF_FEATURES		.EQU	(FLASH_ADDR+1)	; W
+CF_ERROR		.EQU	(FLASH_ADDR+1)	; R
+CF_SECCOUNT		.EQU	(FLASH_ADDR+2)	; W
+
+CF_SECTOR		.EQU	(FLASH_ADDR+3)	; W
+CF_CYL_LOW		.EQU	(FLASH_ADDR+4)	; W
+CF_CYL_HI		.EQU	(FLASH_ADDR+5)	; W
+CF_HEAD			.EQU	(FLASH_ADDR+6)	; W
+
+CF_LBA0			.EQU	(FLASH_ADDR+3)	; W
+CF_LBA1			.EQU	(FLASH_ADDR+4)	; W
+CF_LBA2			.EQU	(FLASH_ADDR+5)	; W
+CF_LBA3			.EQU	(FLASH_ADDR+6)	; W
+
+CF_STATUS		.EQU	(FLASH_ADDR+7)	; R
+CF_COMMAND		.EQU	(FLASH_ADDR+7)	; W
+
+;CF Features
+CF_8BIT			.EQU	1
+CF_NOCACHE		.EQU	082H
+;CF Commands
+CF_READ_SEC		.EQU	020H
+CF_WRITE_SEC	.EQU	030H
+CF_SET_FEAT		.EQU 	0EFH
+
+LF				.EQU	0AH		;line feed
+FF				.EQU	0CH		;form feed
+CR				.EQU	0DH		;carriage RETurn
+
+;================================================================================================
+
+		.ORG	0
+
+		CALL	cfWait
+		LD 	A,CF_8BIT	; Set IDE to be 8bit
+		OUT	(CF_FEATURES),A
+		LD	A,CF_SET_FEAT
+		OUT	(CF_COMMAND),A
+
+
+		CALL	cfWait
+		LD 	A,CF_NOCACHE	; No write cache
+		OUT	(CF_FEATURES),A
+		LD	A,CF_SET_FEAT
+		OUT	(CF_COMMAND),A
+
+		LD	B,numSecs
+
+		LD	A,0
+		LD	(secNo),A
+		LD	HL,loadAddr
+		LD	(dmaAddr),HL
+processSectors:
+
+		CALL	cfWait
+
+		LD	A,(secNo)
+		OUT 	(CF_LBA0),A
+		LD	A,0
+		OUT 	(CF_LBA1),A
+		OUT 	(CF_LBA2),A
+		LD	a,0E0H
+		OUT 	(CF_LBA3),A
+		LD 	A,1
+		OUT 	(CF_SECCOUNT),A
+
+		call	read
+
+		LD	DE,0200H
+		LD	HL,(dmaAddr)
+		ADD	HL,DE
+		LD	(dmaAddr),HL
+		LD	A,(secNo)
+		INC	A
+		LD	(secNo),A
+
+		djnz	processSectors
+
+
+		jp	BIOS
+
+;------------------------------------------------------------------------------
+; Read physical sector from host
+
+read:
+		PUSH 	AF
+		PUSH 	BC
+		PUSH 	HL
+
+		CALL 	cfWait
+
+		LD 	A,CF_READ_SEC
+		OUT 	(CF_COMMAND),A
+
+		CALL 	cfWait
+
+		LD 	c,4
+		LD 	HL,(dmaAddr)
+rd4secs:
+		LD 	b,128
+rdByte:
+		nop
+		nop
+		in 	A,(CF_DATA)
+		LD 	(HL),A
+		iNC 	HL
+		dec 	b
+		JR 	NZ, rdByte
+		dec 	c
+		JR 	NZ,rd4secs
+
+		POP 	HL
+		POP 	BC
+		POP 	AF
+
+		RET
+
+
+; Wait for disk to be ready (busy=0,ready=1)
+cfWait:
+		PUSH 	AF
+cfWait1:
+		in 	A,(CF_STATUS)
+		AND 	080H
+		cp 	080H
+		JR	Z,cfWait1
+		POP 	AF
+		RET
+
+;------------------------------------------------------------------------------
+		.ORG	4000H
+
+secNo		.ds	1
+dmaAddr		.ds	2
+
+		.END
