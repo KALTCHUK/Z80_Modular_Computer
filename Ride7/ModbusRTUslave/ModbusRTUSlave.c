b5dfeb304972eacb25264da36c9fd4fb94115f12
Ricardo Kaltchuk <kaltchuk@gmail.com>
2022-09-20 08:21:27 +0300

    modbus

--- Ride7/ModbusRTUslave/ModbusRTUSlave.c
+++ Ride7/ModbusRTUslave/ModbusRTUSlave.c
@@ -1,24 +1,22 @@
 /*
-Base for 2 modbus remote I/O cards - 16 digital inputs and 8 digital outputs.
+Base for 2 modbus remote I/O cards - DI-16 (16 digital inputs) and DO-8 (8 digital outputs).
  
 Port1 is connected to the I/O bus. P1.0 and P1.1 need external pullup resistors.
 
 Port3 has several especial pins:
-
-P3.0: RX
-P3.1: TX
-P3.2: not used
-P3.3: not used
-P3.4: SEL0 (select lower word of digital input)
-P3.5: SEL1 (select higher word of digital input)
-P3.7: DE (RS485 drive enable)
-
+	P3.0: RX
+	P3.1: TX
+	P3.2: not used
+	P3.3: pinFactory (use factory set id and baud rate if signal low)
+	P3.4: SEL0 (select lower word of digital input)
+	P3.5: SEL1 (select higher word of digital input)
+	P3.7: pinDE (RS485 drive enable)
 
 */
 
 void timer0_isr() interrupt 1
 {
-    TH0 = 0xfc;        //Reload the timer value.
+    TH0 = 0xfc;        //Reload the TIMER0.
     TL0 = 0x66;
 
     milli++;
@@ -32,40 +30,39 @@ void milliStart(void) {
     TR0 = 1;           //Turn ON TIMER0.
     ET0 = 1;           //Enable TIMER0 Interrupt.
     EA = 1;            //Enable Global Interrupt.
+	
 	milli = 0;
 }
 
-void ModbusRTUslaveBegin(uint8_t id, uint32_t baud, uint8_t config) {
+void ModbusRTUslaveBegin(char id, unsigned int baud) {
   if (baud >= 19200) {
-    _charTimeout = 1;
-    _frameTimeout = 2;
+    charTimeout = 1;
+    frameTimeout = 2;
   }
   else {
-    _charTimeout = 15000/baud;
-    _frameTimeout = 35000/baud;
-  }
-
-  if (_dePin != 255) {		// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-    digitalWrite(_dePin, LOW);
-    pinMode(_dePin, OUTPUT);
+    charTimeout = 15000/baud;
+    frameTimeout = 35000/baud;
   }
   
+  milliStart();
   do {
     if (RI != 0) {
 		milliStart();
 		RI = 0;
     }
-  } while (milli < _frameTimeout);
+  } while (milli < frameTimeout);
 }
 
 void ModbusRTUslavePoll() {
   if (RI != 0) {				// serial read available?
-    uint8_t i = 0;
-    uint32_t startTime;
+    char i = 0;
+	
+    milliStart();
     do {
-      if (Serial.available() > 0) {
-        startTime = micros();
-        _buf[i] = Serial.read();
+      if (RI != 0) {
+        milliStart();
+        if(i < MAXBUF)	buf[i] = serialRX();
+		else			RI = 0;
         i++;
       }
     } while (micros() - startTime < _charTimeout && i < _bufSize);
@@ -129,77 +126,78 @@ void ModbusRTUslavePoll() {
             else {
               for (uint8_t j = 0; j < quantity; j++) {
                 if (!_holdingRegisterWrite(startAddress + j, _bytesToWord(_buf[j * 2 + 7], _buf[j * 2 + 8]))) {
-                  _exceptionResponse(4);
+                  exceptionResponse(4);
                   return;
                 }
               }
-              _write(6);
+              write(6);
             }
           }
           break;
         default:
-          _exceptionResponse(1);
+          exceptionResponse(1);
           break;
       }
     }
   }
 }
 
-void processBoolRead(uint16_t numBools, BoolRead boolRead) {
-  uint16_t startAddress = _bytesToWord(_buf[2], _buf[3]);
-  uint16_t quantity = _bytesToWord(_buf[4], _buf[5]);
-  if (quantity == 0 || quantity > ((_bufSize - 6) * 8)) _exceptionResponse(3);
-  else if ((startAddress + quantity) > numBools) _exceptionResponse(2);
+void processBoolRead(unsigned int numBools) {
+  unsigned int startAddress = _bytesToWord(buf[2], buf[3]);
+  unsigned int quantity = _bytesToWord(buf[4], buf[5]);
+  if (quantity == 0 || quantity > ((bufSize - 6) * 8)) exceptionResponse(3);
+  else if ((startAddress + quantity) > numBools) exceptionResponse(2);
   else {
     for (uint8_t j = 0; j < quantity; j++) {
       int8_t value = boolRead(startAddress + j);
       if (value < 0) {
-        _exceptionResponse(4);
+        exceptionResponse(4);
         return;
       }
-      bitWrite(_buf[3 + (j >> 3)], j & 7, value);
+      bitWrite(buf[3 + (j >> 3)], j & 7, value);
     }
-    _buf[2] = _div8RndUp(quantity);
-    _write(3 + _buf[2]);
+    buf[2] = div8RndUp(quantity);
+    write(3 + buf[2]);
   }
 }
 
-void processWordRead(uint16_t numWords, WordRead wordRead) {
-  uint16_t startAddress = _bytesToWord(_buf[2], _buf[3]);
-  uint16_t quantity = _bytesToWord(_buf[4], _buf[5]);
-  if (quantity == 0 || quantity > ((_bufSize - 6) >> 1)) _exceptionResponse(3);
-  else if ((startAddress + quantity) > numWords) _exceptionResponse(2);
+void processWordRead(unsigned int numWords) {
+  unsigned int startAddress = bytesToWord(buf[2], buf[3]);
+  unsigned int quantity = bytesToWord(buf[4], buf[5]);
+  if (quantity == 0 || quantity > ((bufSize - 6) >> 1)) exceptionResponse(3);
+  else if ((startAddress + quantity) > numWords) exceptionResponse(2);
   else {
-    for (uint8_t j = 0; j < quantity; j++) {
-      int32_t value = wordRead(startAddress + j);
+    for (char j = 0; j < quantity; j++) {
+      long value = wordRead(startAddress + j);
       if (value < 0) {
-        _exceptionResponse(4);
+        exceptionResponse(4);
         return;
       }
-      _buf[3 + (j * 2)] = highByte(value);
-      _buf[4 + (j * 2)] = lowByte(value);
+      buf[3 + (j * 2)] = highByte(value);
+      buf[4 + (j * 2)] = lowByte(value);
     }
-    _buf[2] = quantity * 2;
-    _write(3 + _buf[2]);
+    buf[2] = quantity * 2;
+    write(3 + buf[2]);
   }
 }
 
-void exceptionResponse(uint8_t code) {
-  _buf[1] |= 0x80;
-  _buf[2] = code;
-  _write(3);
+void exceptionResponse(char code) {
+  buf[1] |= 0x80;
+  buf[2] = code;
+  write(3);
 }
 
-void write(uint8_t len) {
-  delay(responseDelay);
+void write(char len) {
+  
   if (buf[0] != 0) {
-    uint16_t crc = _crc(len);
+    unsigned int crc = crc(len);
+	
     buf[len] = lowByte(crc);
     buf[len + 1] = highByte(crc);
-    digitalWrite(_dePin, HIGH);
-    Serial.write(_buf, len + 2);
-    Serial.flush();
-    digitalWrite(_dePin, LOW);
+
+    pinDE = 1;
+    for(char i=0; i<= len+2; i++)	serialTX(buf[i]);
+    pinDE = 0;
   }
 }
 
@@ -225,3 +223,4 @@ unsigned int div8RndUp(unsigned int value) {
 unsigned int bytesToWord(char high, char low) {
   return (high << 8) | low;
 }
+
