5f4eca283e49fd41556942a8f4cd1581fa0b5541
Ricardo Kaltchuk <kaltchuk@gmail.com>
2022-09-22 08:58:25 +0300

    modbus

---
+++ Ride7/DO-8/DO-8.c
@@ -0,0 +1,34 @@
+/*
+ DO-8.c - Modbus RTU Slave controller (AT89C4051) for DO-8 Card. By Kaltchuk, Sep/2022.
+*/
+
+#include <REG51.h>
+#include "Serial.h"
+#include "EEPROM.h"
+#include "ModbusSlave.h"
+
+// Definitions
+#define baudEEaddr		0	// Baud rate address in EEPROM (register 1).
+#define idEEaddr		2	// ID address in EEPROM (register 2).
+
+void main() {
+	unsigned int id;
+	
+	numCoils = 8;
+	numDiscreteInputs = 0;
+	numHoldingRegisters = 2;
+	
+	baud = 9600;	// factory set baud rate.
+	id = 7;			// factory set modbus slave id.
+
+	if (_FS == 1) {
+		baud = EEPROMread(baudEEaddr);
+		id =  EEPROMread(idEEaddr);
+	}
+	serialInit(baud);
+	modbusBegin(id, baud);
+	
+	while (1) {
+		modbusPoll();
+	}
+}
--- Ride7/DO-8/Serial.h
+++ Ride7/DO-8/Serial.h
@@ -1,5 +1,19 @@
+/*
+ Serial.h - Header file for serial communication via RS485 - Specifically for use with Modbus.
+
+ Functions:
+	void serialInit(unsigned int baud) - Initialize UART 8N1 @ baud (crystal=11.059MHz).
+	void serialTX(char x) - Send one byte.
+	unsigned char serialRX(void) - Receive one byte.
+
+Port3 has two especial pins:
+	P3.0: RX
+	P3.1: TX
+
+*/
+
 // Global Variables
-unsigned int baud = 9600;
+unsigned int baud;
 
 // Function Prototyping
 void serialInit(unsigned int baud);
@@ -8,7 +22,7 @@ unsigned char serialRX(void);
 
 // Functions
 void serialInit(unsigned int baud) {     // Initialize UART
-    TMOD |= 0x20;       // TIMER0 = mode_1 and TIMER1 = mode_2
+    TMOD |= 0x20;       // TIMER1 = mode_2
     PCON |= 0x80;       // SMOD=1 => double baud rate;
     SCON = 0x50;        // Serial port = mode 1 (8 bits, clocked by TIMER1)
 	switch(baud) {
